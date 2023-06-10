7d1feb3b98c68d69f76399b9346781b06fe3398b
Mdromi <mdromi14@gmail.com>
2023-06-10 23:17:26 +0600

    learn Maps

---
+++ golang/9.2-maps.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"sort"
+)
+
+func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+	existingUser, ok := users[name]
+	if !ok {
+		return false, errors.New("not found")
+	}
+	if existingUser.scheduledForDeletion {
+		delete(users, name)
+		return true, nil
+	}
+	return false, nil
+}
+
+// don't touch below this line
+
+type user struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
+}
+func test(users map[string]user, name string) {
+	fmt.Printf("Attempting to delete %s...\n", name)
+	defer fmt.Println("====================================")
+	deleted, err := deleteIfNecessary(users, name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if deleted {
+		fmt.Println("Deleted:", name)
+		return
+	}
+	fmt.Println("Did not delete:", name)
+}
+
+// 9.5
+func getCounts(userIDs []string) map[string]int {
+	counts := make(map[string]int)
+	for _, userId := range userIDs {
+		count := counts[userId]
+		count++
+		counts[userId] = count
+	}
+	return counts
+}
+func test5(userIDs []string, ids []string) {
+	fmt.Printf("Generating counts for %v user IDs...\n", len(userIDs))
+
+	counts := getCounts(userIDs)
+	fmt.Println("Counts from select IDs:")
+	for _, k := range ids {
+		v := counts[k]
+		fmt.Printf(" - %s: %d\n", k, v)
+	}
+	fmt.Println("=====================================")
+}
+
+// 9.10
+func getNameCounts(names []string) map[rune]map[string]int {
+	counts := make(map[rune]map[string]int)
+	for _, name := range names {
+		if name == "" {
+			continue
+		}
+		firstChar := rune(name[0])
+		_, ok := counts[firstChar]
+		if !ok {
+			counts[firstChar] = make(map[string]int)
+		}
+		counts[firstChar][name]++
+	}
+	return counts
+}
+
+func test10(names []string, initial rune, name string) {
+	fmt.Printf("Generating counts for %v names...\n", len(names))
+
+	nameCounts := getNameCounts(names)
+	count := nameCounts[initial][name]
+	fmt.Printf("Count for [%c][%s]: %d\n", initial, name, count)
+	fmt.Println("=====================================")
+}
+
+func main() {
+	users := map[string]user{
+		"john": {
+			name:                 "john",
+			number:               18965554631,
+			scheduledForDeletion: true,
+		},
+		"elon": {
+			name:                 "elon",
+			number:               19875556452,
+			scheduledForDeletion: true,
+		},
+		"breanna": {
+			name:                 "breanna",
+			number:               98575554231,
+			scheduledForDeletion: false,
+		},
+		"kade": {
+			name:                 "kade",
+			number:               10765557221,
+			scheduledForDeletion: false,
+		},
+	}
+	test(users, "john")
+	test(users, "musk")
+	test(users, "santa")
+	test(users, "kade")
+
+	keys := []string{}
+	for name := range users {
+		keys = append(keys, name)
+	}
+	sort.Strings(keys)
+
+	fmt.Println("Final map keys:")
+	for _, name := range keys {
+		fmt.Println(" - ", name)
+	}
+
+	// 9.5
+	userIDs := []string{}
+	for i := 0; i < 10000; i++ {
+		h := md5.New()
+		io.WriteString(h, fmt.Sprint(i))
+		key := fmt.Sprintf("%x", h.Sum(nil))
+		userIDs = append(userIDs, key[:2])
+	}
+
+	test5(userIDs, []string{"00", "ff", "dd"})
+	test5(userIDs, []string{"aa", "12", "32"})
+	test5(userIDs, []string{"bb", "33"})
+
+	// 9.10
+	test10(getNames(50), 'M', "Matthew")
+	test10(getNames(100), 'G', "George")
+	test10(getNames(150), 'D', "Drew")
+	test10(getNames(200), 'P', "Philip")
+	test10(getNames(250), 'B', "Bryant")
+	test10(getNames(300), 'M', "Matthew")
+}
+
+func getNames(length int) []string {
+	names := []string{
+		"Grant", "Eduardo", "Peter", "Matthew", "Matthew", "Matthew", "Peter", "Peter", "Henry", "Parker", "Parker", "Parker", "Collin", "Hayden", "George", "Bradley", "Mitchell", "Devon", "Ricardo", "Shawn", "Taylor", "Nicolas", "Gregory", "Francisco", "Liam", "Kaleb", "Preston", "Erik", "Alexis", "Owen", "Omar", "Diego", "Dustin", "Corey", "Fernando", "Clayton", "Carter", "Ivan", "Jaden", "Javier", "Alec", "Johnathan", "Scott", "Manuel", "Cristian", "Alan", "Raymond", "Brett", "Max", "Drew", "Andres", "Gage", "Mario", "Dawson", "Dillon", "Cesar", "Wesley", "Levi", "Jakob", "Chandler", "Martin", "Malik", "Edgar", "Sergio", "Trenton", "Josiah", "Nolan", "Marco", "Drew", "Peyton", "Harrison", "Drew", "Hector", "Micah", "Roberto", "Drew", "Brady", "Erick", "Conner", "Jonah", "Casey", "Jayden", "Edwin", "Emmanuel", "Andre", "Phillip", "Brayden", "Landon", "Giovanni", "Bailey", "Ronald", "Braden", "Damian", "Donovan", "Ruben", "Frank", "Gerardo", "Pedro", "Andy", "Chance", "Abraham", "Calvin", "Trey", "Cade", "Donald", "Derrick", "Payton", "Darius", "Enrique", "Keith", "Raul", "Jaylen", "Troy", "Jonathon", "Cory", "Marc", "Eli", "Skyler", "Rafael", "Trent", "Griffin", "Colby", "Johnny", "Chad", "Armando", "Kobe", "Caden", "Marcos", "Cooper", "Elias", "Brenden", "Israel", "Avery", "Zane", "Zane", "Zane", "Zane", "Dante", "Josue", "Zackary", "Allen", "Philip", "Mathew", "Dennis", "Leonardo", "Ashton", "Philip", "Philip", "Philip", "Julio", "Miles", "Damien", "Ty", "Gustavo", "Drake", "Jaime", "Simon", "Jerry", "Curtis", "Kameron", "Lance", "Brock", "Bryson", "Alberto", "Dominick", "Jimmy", "Kaden", "Douglas", "Gary", "Brennan", "Zachery", "Randy", "Louis", "Larry", "Nickolas", "Albert", "Tony", "Fabian", "Keegan", "Saul", "Danny", "Tucker", "Myles", "Damon", "Arturo", "Corbin", "Deandre", "Ricky", "Kristopher", "Lane", "Pablo", "Darren", "Jarrett", "Zion", "Alfredo", "Micheal", "Angelo", "Carl", "Oliver", "Kyler", "Tommy", "Walter", "Dallas", "Jace", "Quinn", "Theodore", "Grayson", "Lorenzo", "Joe", "Arthur", "Bryant", "Roman", "Brent", "Russell", "Ramon", "Lawrence", "Moises", "Aiden", "Quentin", "Jay", "Tyrese", "Tristen", "Emanuel", "Salvador", "Terry", "Morgan", "Jeffery", "Esteban", "Tyson", "Braxton", "Branden", "Marvin", "Brody", "Craig", "Ismael", "Rodney", "Isiah", "Marshall", "Maurice", "Ernesto", "Emilio", "Brendon", "Kody", "Eddie", "Malachi", "Abel", "Keaton", "Jon", "Shaun", "Skylar", "Ezekiel", "Nikolas", "Santiago", "Kendall", "Axel", "Camden", "Trevon", "Bobby", "Conor", "Jamal", "Lukas", "Malcolm", "Zackery", "Jayson", "Javon", "Roger", "Reginald", "Zachariah", "Desmond", "Felix", "Johnathon", "Dean", "Quinton", "Ali", "Davis", "Gerald", "Rodrigo", "Demetrius", "Billy", "Rene", "Reece", "Kelvin", "Leo", "Justice", "Chris", "Guillermo", "Matthew", "Matthew", "Matthew", "Kevon", "Steve", "Frederick", "Clay", "Weston", "Dorian", "Hugo", "Roy", "Orlando", "Terrance", "Kai", "Khalil", "Khalil", "Khalil", "Graham", "Noel", "Willie", "Nathanael", "Terrell", "Tyrone",
+	}
+	if length > len(names) {
+		length = len(names)
+	}
+	return names[:length]
+}
