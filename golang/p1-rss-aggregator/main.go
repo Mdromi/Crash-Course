38a0856042b69dc6d8885f8f6dec741487524bd8
mdromi <mdromi14@gmail.com>
2023-06-26 15:22:07 -0400

    compleate rss-aggregator project

--- golang/p1-rss-aggregator/main.go
+++ golang/p1-rss-aggregator/main.go
@@ -3,6 +3,7 @@ package main
 // go build -o rssag && ./rssag
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,10 +12,17 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+	"github.com/mdromi/p1-rss-aggregator/internal/database"
 )
 
-func main ()  {
-	
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func main() {
+
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
 
@@ -22,32 +30,60 @@ func main ()  {
 		log.Fatal("PORT is not found in the environment")
 	}
 
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database:", err)
+	}
+
+	apiConfig := apiConfig{
+		DB: database.New(conn),
+	}
+
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 		AllowedOrigins:   []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
-	
+
 	v1Router := chi.NewRouter()
+
+	// Create and get user
+	v1Router.Post("/users", apiConfig.handlerUsersCreate)
+	v1Router.Get("/users", apiConfig.middlewareAuth(apiConfig.handlerGetUser))
+
+	// Create and get feeds
+	v1Router.Post("/feeds", apiConfig.middlewareAuth(apiConfig.handlerFeedCreate))
+	v1Router.Get("/feeds", apiConfig.handlerGetFeeds)
+
+	// Get feeds and create feed
+	v1Router.Post("/feed_follows", apiConfig.middlewareAuth(apiConfig.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiConfig.middlewareAuth(apiConfig.handlerFeedFollowsGet))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiConfig.middlewareAuth(apiConfig.handlerFeedFollowDelete))
+
+	v1Router.Get("/posts", apiConfig.middlewareAuth(apiConfig.handlerPostsGet))
+
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 
 	router.Mount("/v1", v1Router)
-	
+
 	srv := &http.Server{
 		Handler: router,
-		Addr: ":" + portString,
+		Addr:    ":" + portString,
 	}
 
 	log.Printf("Server starting on port %v", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
